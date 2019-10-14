3267caacc42ba71412326375c593f3cb499f939a
zhanghw <908171224@qq.com>
2019-10-14 14:11:01 +0800

    v1.2

--- src/main/java/com/baizhi/CmfzApplication.java
+++ src/main/java/com/baizhi/CmfzApplication.java
@@ -12,6 +12,7 @@ public class CmfzApplication {
         SpringApplication.run(CmfzApplication.class, args);
         System.out.println("张三");
         System.out.println("李四");
+        System.out.println("赵六");
     }
 
 }
