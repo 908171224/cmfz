f4c83a5fcb4b644ed4e070c6a7c95607265b5a56
zhanghw <908171224@qq.com>
2019-10-21 22:03:19 +0800

    v1.5

--- src/main/java/com/baizhi/serviceImpl/ArticleServiceImpl.java
+++ src/main/java/com/baizhi/serviceImpl/ArticleServiceImpl.java
@@ -1,9 +1,23 @@
 package com.baizhi.serviceImpl;
 
+import com.baizhi.ArticleRepsitory.ArticleRepsitory;
 import com.baizhi.entity.Article;
 import com.baizhi.mapper.ArticleMapper;
 import com.baizhi.service.ArticleService;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +29,10 @@ import java.util.*;
 public class ArticleServiceImpl implements ArticleService {
     @Autowired
     private ArticleMapper articleMapper;
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+    @Autowired
+    private ArticleRepsitory articleRepsitory;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -49,5 +67,61 @@ public class ArticleServiceImpl implements ArticleService {
     @Override
     public void update(Article article) {
         articleMapper.update(article);
+    }
+
+    @Override
+    public List<Article> queryByes(String val) {
+        List<Article> all = articleMapper.findAll();
+        for (Article article : all) {
+            articleRepsitory.save(article);
+        }
+
+        HighlightBuilder.Field field = new HighlightBuilder.Field("*");
+        field.preTags("<span style='color:red'>");
+        field.postTags("</span>");
+        field.requireFieldMatch(false);
+
+
+        NativeSearchQuery build = new NativeSearchQueryBuilder()
+                .withIndices("cmfz")
+                .withTypes("article")
+                .withQuery(QueryBuilders.queryStringQuery(val).analyzer("ik_max_word").field("title").field("content"))
+                .withHighlightFields(field)
+                .build();
+
+        AggregatedPage<Article> articles = elasticsearchTemplate.queryForPage(build, Article.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse searchResponse, Class<T> aClass, Pageable pageable) {
+
+                List<Article> list = new ArrayList<>();
+
+                SearchHits hits = searchResponse.getHits();
+                SearchHit[] hits1 = hits.getHits();
+
+                for (SearchHit searchHit : hits1) {
+                    Article article = new Article();
+                    article.setId(searchHit.getSourceAsMap().get("id").toString());
+                    article.setAuthor(searchHit.getSourceAsMap().get("author").toString());
+                    article.setTitle(searchHit.getSourceAsMap().get("title").toString());
+                    article.setContent(searchHit.getSourceAsMap().get("content").toString());
+                    article.setTitle(searchHit.getSourceAsMap().get("title").toString());
+                    /*article.setCreateDate(new Date(Long.valueOf(searchHit.getSourceAsMap().get("createDate").toString())));*/
+                    article.setStatus(searchHit.getSourceAsMap().get("status").toString());
+
+                    Map<String, HighlightField> highlightFields = searchHit.getHighlightFields();
+                    if (highlightFields.get("title") != null) {
+                        String title = highlightFields.get("title").getFragments()[0].toString();
+                        article.setTitle(title);
+                    }
+                    if (highlightFields.get("content") != null) {
+                        String content = highlightFields.get("content").getFragments()[0].toString();
+                        article.setContent(content);
+                    }
+                    list.add(article);
+                }
+                return new AggregatedPageImpl<T>((List<T>) list);
+            }
+        });
+        return articles.getContent();
     }
 }
